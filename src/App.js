cfb474febdf86acefb5831884999cc8470f858fe
fallinnadim <nadim@mhs.mdp.ac.id>
2023-12-30 16:28:55 +0700

    hehe lupa

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    var date1 = new Date();
+const App = () => {
+  var date1 = new Date();
     var date2 = new Date("02/14/2024");
     var timeDiff = Math.abs(date2.getTime() - date1.getTime());
     var diffSeconds = Math.ceil(timeDiff / 1000); 
@@ -12,8 +11,8 @@ class App extends Component {
     useEffect(() => {
       counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
     }, [counter]);
-    return (
-      <div className="App">
+  return (
+    <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">I Love Ribeye</h1>
@@ -23,8 +22,7 @@ class App extends Component {
           asdf <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
-  }
+  )
 }
 
-export default App;
+export default App
