70f1ac1bc44238d503c2730a22c43998221668e3
fallinnadim <nadim@mhs.mdp.ac.id>
2023-12-30 16:14:37 +0700

    hujan sore sore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    var date1 = new Date();
+    var date2 = new Date("02/14/2024");
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffSeconds = Math.ceil(timeDiff / 1000); 
+    const [counter, setCounter] = useState(diffSeconds);
+    useEffect(() => {
+      counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }, [counter]);
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +19,7 @@ class App extends Component {
           <h1 className="App-title">I Love Ribeye</h1>
         </header>
         <p className="App-intro">
+          <div>Countdown: {counter}</div>
           asdf <code>src/App.js</code> and save to reload.
         </p>
       </div>
