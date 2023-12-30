e3445b37688c170778d7364944d882187f656a97
fallinnadim <nadim@mhs.mdp.ac.id>
2023-12-30 16:32:30 +0700

    Test Git WebHook

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   var date1 = new Date();
     var date2 = new Date("02/14/2024");
     var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    var diffSeconds = Math.ceil(timeDiff / 1000); 
+    var diffSeconds = Math.ceil(timeDiff / 60000); 
     const [counter, setCounter] = useState(diffSeconds);
     useEffect(() => {
       counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
@@ -15,11 +15,11 @@ const App = () => {
     <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">I Love Ribeye</h1>
+          <h1 className="App-title">Bukan Valentine kak</h1>
         </header>
         <p className="App-intro">
-          <div>Countdown: {counter}</div>
-          asdf <code>src/App.js</code> and save to reload.
+          <div>Countdown: {counter} menit</div>
+          menuju Pemilu, Coblos dengan Bijak !!!
         </p>
       </div>
   )
